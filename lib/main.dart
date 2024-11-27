9d7f6f4568e6ea477b68721e695a691072f0ec36
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-11-27 10:49:36 +0500

    Custom container

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,65 @@ class StatelessDastur extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.amber),
-        home: const ContainerExample());
+      theme: ThemeData(primaryColor: Colors.amber),
+      home: const HomeWidget(),
+    );
   }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Stateless Widget misoli"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            customContainer(text: "Bu stateless 2", color: Colors.red),
+            customContainer(text: "Salom, Flutter", color: Colors.yellow),
+            customContainer(text: "Bu stateless Widget", color: Colors.green),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget customContainer({
+  required String text,
+  required Color color,
+}) {
+  return Container(
+    padding: const EdgeInsets.all(10),
+    width: 150,
+    height: 150,
+    alignment: Alignment.center,
+    margin: const EdgeInsets.only(top: 10),
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      border: Border.all(color: Colors.black45, width: 2),
+      // borderRadius: BorderRadius.circular(10),
+      boxShadow: const [
+        BoxShadow(
+          blurRadius: 4,
+          color: Colors.grey,
+          offset: Offset(5, 6),
+        ),
+      ],
+      color: color,
+    ),
+    child: Text(
+      text,
+      textAlign: TextAlign.center,
+    ),
+  );
 }
 
 Widget appBarText(String title) {
